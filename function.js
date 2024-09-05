bc6baa6f5fbc48388e2b87bbab7b68313c9b5ef6
imrancoder10 <imranhossain1148@gmail.com>
2024-09-05 14:52:50 +0600

    function init

---
+++ function.js
@@ -0,0 +1,30 @@
+const price = 45;
+
+function fanOffKor(){
+    console.log('bosa thaka utha dara');
+    console.log('walk to the switch.');
+    console.log('click the switch to off of the Fan');
+}
+// call the function
+fanOffKor();
+
+console.log('-----------------------');
+
+
+// function name(){
+
+// }
+
+function brushTeet(){
+    console.log('pick up the brush.');
+    console.log('add paste');
+    console.log('2 min up and down the brush.');
+}
+
+//call the function
+brushTeet();
+
+console.log('----------------------');
+console.log('----------------------');
+console.log('----------------------');
+brushTeet();
