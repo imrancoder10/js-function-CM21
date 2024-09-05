8e5f730460736639972f91abc0c0ee63a86db667
imrancoder10 <imranhossain1148@gmail.com>
2024-09-05 15:18:07 +0600

    parameter

---
+++ parameter.js
@@ -0,0 +1,47 @@
+function vatKhao(){
+    console.log('hand wash kora aso.');
+    console.log('boso');
+    console.log('plate a food neo');
+    console.log('eat');
+
+}
+
+// vatKhao();
+
+
+// 3**2=9
+// 4**2=16
+// 5**2=25
+// 11**2=121
+
+
+function square(number){
+    console.log('value of the number parameter', number);
+    const borgo = number * number;
+    console.log('square of the given number is: ', borgo);
+}
+
+square(4);
+console.log('-------------------------------');
+square(12);
+console.log('----------------------');
+square(5);
+
+
+function add(num1, num2){
+    const sum = num1 + num2;
+    console.log('the sum of ', num2, 'and', num2, 'is: ',sum);
+}
+add(5, 71);
+
+
+function addAll(a,b,c,d,e){
+    const total = a+b+c+d+e;
+    console.log(a, b, c, d, e);
+    console.log(total);
+}
+
+addAll(3, 5, 8, 41);
+addAll(3, 5, 4, 9, 4)
+
+
