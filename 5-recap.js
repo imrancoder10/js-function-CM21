6175b024008a51201addfba4947aa85abbe67c00
imrancoder10 <imranhossain1148@gmail.com>
2024-09-06 18:10:33 +0600

    recap

---
+++ 5-recap.js
@@ -0,0 +1,32 @@
+function add(price1, price2){
+    const total = price1 + price2;
+    return total;
+}
+const bill = add(5, 80);
+
+// shortcut return
+function add2(price1, price2){
+    return price1 + price2
+}
+const bill2 = add2(25, 15);
+
+console.log(bill, bill2);
+
+// =======================================================
+
+function doMath(num1, num2){
+    const sum = num1 + num2;
+    const diff = num1 - num2;
+    const multiply = sum * diff;
+    const result = multiply / 2;
+    return result;
+}
+
+const result = doMath(10, 5);
+ console.log(result);
+
+
+//  ==========================================
+function isEven(number){
+    
+}
